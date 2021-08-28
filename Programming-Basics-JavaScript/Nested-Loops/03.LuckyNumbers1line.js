ab118c196e9d6896604102e897755a1fb1dc2f75
Alex Gaydarova <alexandrina.gaydarova@gmail.com>
2021-08-28 13:19:21 +0300

    Add Lucky Numbers JavaScript Nested Loops Code

---
+++ Programming-Basics-JavaScript/Nested-Loops/03.LuckyNumbers1line.js
@@ -0,0 +1,19 @@
+function luckyNumbers(input) {
+    let n = Number(input[0]);
+    let print = "";
+    for (let i = 1; i <= 9; i++) {
+        for (let j = 1; j <= 9; j++) {
+            for (let k = 1; k <= 9; k++) {
+                for (let l = 1; l <= 9; l++) {
+                    if ((i + j == k + l) && (n % (i + j) == 0)) {
+                        let tempPrint = "" + i + j + k + l + " "; // превръщаме в текст и долепяме, за да не се съберат числата.
+                        print += tempPrint; 
+                    }
+                }
+            }   
+        }
+    }
+    console.log(print);
+}
+
+luckyNumbers([3]);
