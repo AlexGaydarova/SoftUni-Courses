2bdd0dc3757445f538e63accc461d88b7beb199a
Alex Gaydarova <alexandrina.gaydarova@gmail.com>
2021-09-13 14:16:13 +0300

    Add For Loop Code

---
+++ Programming-Basics-JavaScript/For-Loop/01.NumbersFrom1to100.js
@@ -0,0 +1,15 @@
+function numbersNTo100(input) {
+    let num = Number(input[0]);
+    for (let i = num; i <= 100; i++) {
+        console.log(i);
+    }
+}
+//numbersNTo100(["80"]);
+
+function num1To100() {
+    for (i = 1; i <= 100; i++) {
+        console.log(i);
+    }
+}
+
+num1To100();
