4dddccbf773230cd63f711ec691c101a7992944a
Alex Gaydarova <alexandrina.gaydarova@gmail.com>
2021-10-04 16:40:13 +0300

    Add While Loop Code.

---
+++ Programming-Basics-JavaScript/While Loop/04.Numbers-DividedBy3WithoutReminder.js
@@ -0,0 +1,9 @@
+function numbersDiv3WhithoutReminder() {
+    for (let i = 1; i <= 100; i++) {
+        if (i % 3 == 0) {
+            console.log(i);
+        }
+    }
+}
+
+numbersDiv3WhithoutReminder()
