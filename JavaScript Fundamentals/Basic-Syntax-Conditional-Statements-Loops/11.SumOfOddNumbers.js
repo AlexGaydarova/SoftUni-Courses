2e92c5e22f62a08965842c47c30623d19205856f
Alex Gaydarova <alexandrina.gaydarova@gmail.com>
2021-11-06 15:09:06 +0200

    JavaScript Fundamentals - Conditional-Statements-Loops

---
+++ JavaScript Fundamentals/Basic-Syntax-Conditional-Statements-Loops/11.SumOfOddNumbers.js
@@ -0,0 +1,12 @@
+function oddNumber(n) {
+    let sum = 0;
+    let num = 1;
+    for (let i = 0; i < n; i++) {
+        console.log(num);
+        sum += num;
+        num += 2;
+    }
+    console.log(`Sum: ` + sum);
+}
+
+oddNumber(5);
