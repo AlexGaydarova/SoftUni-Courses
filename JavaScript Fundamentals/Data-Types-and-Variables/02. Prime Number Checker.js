fef544c8d9454a432a631b03aba4c03fec797b7f
Alexandrina Gaydarova <86298268+AlexGaydarova@users.noreply.github.com>
2022-09-26 11:38:00 +0300

    Data types and variables

---
+++ JavaScript Fundamentals/Data-Types-and-Variables/02. Prime Number Checker.js
@@ -0,0 +1,11 @@
+function primeNumberChecker(num) {
+    let isPrime = true;
+    for (let i = 2; i < num; i++) {
+        if (num % i === 0 ) {
+            isPrime = false; // не е просто число
+        } 
+    }
+    console.log(isPrime);
+}
+
+primeNumberChecker(81);
